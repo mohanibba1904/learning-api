a686f38bd6981902fe7c9f56fd8cfa3934055b8d
mohanibba1904 <mohanibba526@gmail.com>
2021-12-06 18:01:46 +0530

    third

--- myothermodule/database.py
+++ myothermodule/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mohan@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
